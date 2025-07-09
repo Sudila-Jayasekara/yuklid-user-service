9f11d13242522216a944e8cdf38b3dc90ffe0db2
Sudila-Jayasekara <mpsjayasekara@gmail.com>
2025-07-09 23:07:15 +0530

    project init

---
+++ src/main/java/com/yuklid/user_service/YuklidUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yuklid.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuklidUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuklidUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuklid/user_service/YuklidUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuklid.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuklidUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
